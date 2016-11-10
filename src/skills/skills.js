7378e9871a81dbcd3308aa5d405c39a99003ab9e
Jamey Stiling <jastiling@gmail.com>
2016-11-10 15:45:29 -0800

    dynamic input of skills and goals

--- src/skills/skills.js
+++ src/skills/skills.js
@@ -1,4 +1,8 @@
 export default function ($scope) {
+  let params = {
+    createHasInput: false
+  }
+
   $scope.skills = [
     {
       skill: 'Angular',
@@ -11,5 +15,16 @@ export default function ($scope) {
       progress: '70%',
     }
   ]
+
+  //watches for new skill or goal input and dynamically adds the content in a new row
+  $scope.$watchGroup(['newSkillInput', 'newGoalInput'],(values) => {
+    if (values[0] && !params.createHasInput) {
+      $scope.skills.push({ skill: values[0], goal: values[1] });
+      params.createHasInput = true;
+    } else if (values[0] && params.createHasInput) {
+      $scope.skills[$scope.skills.length - 1].skill = values[0];
+      $scope.skills[$scope.skills.length - 1].goal = values[1];
+    }
+  })
 
 }
