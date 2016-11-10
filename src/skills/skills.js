8dbf0ea7d7e09886f0a9203b5deed33fc187091d
Jamey Stiling <jastiling@gmail.com>
2016-11-10 14:34:27 -0800

    table and progress bar

--- src/skills/skills.js
+++ src/skills/skills.js
@@ -1,3 +1,15 @@
-export default function () {
-  
+export default function ($scope) {
+  $scope.skills = [
+    {
+      skill: 'Angular',
+      goal: '100 hrs',
+      progress: '80%',
+    },
+    {
+      skill: 'React',
+      goal: '200 hrs',
+      progress: '70%',
+    }
+  ]
+
 }
